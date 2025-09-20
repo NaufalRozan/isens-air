a5ef5c4d391e5fe6b813d27d0ad73ec47432df7e
NaufalRozan <rozan_naufal@yahoo.com>
2025-09-20 22:07:52 +0700

    fix: correct spelling of project title in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "iSENSE-Air",
+  title: "iSENS-Air",
   description: "Artificial Intelligence for River Water Quality Monitoring",
 };
 
